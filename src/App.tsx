bc80e6620cee928987524236ffb70c68ec872d83
Soham Sarkar <soham.ss1@gmail.com>
2023-03-14 20:52:01 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import type {Component} from 'solid-js'
+
+import logo from './logo.svg'
+import styles from './App.module.css'
+
+const App: Component = () => {
+  const count = 1
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt='logo' />
+        <p>
+          Edit <code>src/App.tsx</code> and save to {count}.
+        </p>
+        <a
+          class={styles.link}
+          href='https://github.com/solidjs/solid'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
